d9d16f4aeba4dbf075e9d3d45d4b5bca2efbfc69
adam99610 <adamwroot@hotmail.co.uk>
2024-01-13 19:53:28 +0000

    removed stuff and changed stuff

--- Adam'sT-shirtStore/SiteAssets/Scripts/rec.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("customer-rec").style.display = "none";
-
-function toggleSection() {
-    var section = document.getElementById("customer-rec");
-    section.style.display = (section.style.display === "none") ? "block" : "none";
-}
